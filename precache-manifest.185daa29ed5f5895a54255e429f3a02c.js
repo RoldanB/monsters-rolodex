21f11e6f5e5b8bf254e908f436c76565a855e4c6
rbiete <rbiete@uncc.edu>
2020-02-24 23:39:04 -0500

    Updates

---
+++ precache-manifest.185daa29ed5f5895a54255e429f3a02c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18d3dc194b67d6b70edac1c4de8423e4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cf9dbed88d1e40c0529a",
+    "url": "/monsters-rolodex/static/css/main.65f23a99.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf9dbed88d1e40c0529a",
+    "url": "/monsters-rolodex/static/js/main.0fafdb52.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
