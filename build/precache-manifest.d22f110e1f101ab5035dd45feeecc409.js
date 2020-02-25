8af9ad6ebc0f4181f8a5be3ad9f2221965956864
rbiete <rbiete@uncc.edu>
2020-02-24 23:17:09 -0500

    Added search, card, and cardlist w/ styles

---
+++ build/precache-manifest.d22f110e1f101ab5035dd45feeecc409.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b8f7f2c356fafaa1ab6b1e9f83a2364",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f953ad6e2a3f743980ea",
+    "url": "/static/css/main.0c9ce089.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f953ad6e2a3f743980ea",
+    "url": "/static/js/main.2171f517.chunk.js"
+  },
+  {
+    "revision": "8963f78c0e1a10b5862f",
+    "url": "/static/js/runtime-main.8b07e775.js"
+  }
+]);
